cd1f537939ad119c1b88a563072bda797a28879f
Ilan Okker <ilan.okker@gmail.com>
2023-02-22 12:16:29 +0100

    validations added

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ApplicationRecord
-  belongs_to :restauarant
+  belongs_to :restaurant
+
+  validates :content, :rating, presence: true
+  validates :rating, only_integer: true
+  validates :rating, length: { in: 1..5 }
 end
